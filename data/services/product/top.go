6e85bf6377be990c237f9037222a14553dbba01f
Demetrios Kopatsis <jkopatsis@gmail.com>
2024-11-14 17:28:13 -0800

    forgot save

--- data/services/product/top.go
+++ data/services/product/top.go
@@ -5,5 +5,9 @@ import "beam/data/models"
 func CreateTopWords(forTop models.AllFilters, query string) models.TopWords {
 	ret := models.TopWords{}
 
+	if query != "" {
+		ret.Query = `Matching Search: "` + query + `"`
+	}
+
 	return ret
 }
