d532d9463ada307dee650ec15ed30ab9fd44152c
Demetrios Kopatsis <jkopatsis@gmail.com>
2025-01-14 17:23:14 -0800

    add fields

--- data/models/review.go
+++ data/models/review.go
@@ -1,11 +1,16 @@
 package models
 
+import "time"
+
 type Review struct {
-	PK        int `gorm:"primaryKey"`
-	UserID    int `gorm:"index"`
-	ProductID int `gorm:"index"`
-	Stars     int
-	JustStar  bool
-	Subject   string
-	Body      string
+	PK         int       `gorm:"primaryKey"`
+	UserID     int       `gorm:"index"`
+	ProductID  int       `gorm:"index"`
+	CreatedAt  time.Time `gorm:"index"`
+	Status     string    `gorm:"index"`
+	LastEdited time.Time
+	Stars      int
+	JustStar   bool
+	Subject    string
+	Body       string
 }
