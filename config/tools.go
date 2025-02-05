669e7f3294bed721e2f9f21037754edc9d9e93dc
James Kopatsis <jkopatsis@gmail.com>
2025-02-04 19:55:30 -0800

    currency conversion rates code

--- config/tools.go
+++ config/tools.go
@@ -1,10 +1,14 @@
 package config
 
 import (
+	"beam/data/models"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/oschwald/geoip2-golang"
@@ -58,4 +62,89 @@ func (t *Tools) initializeGeo() {
 		t.Geo = nil
 	}
 	t.Geo = ipDB
+}
+
+func (t *Tools) getConversionRates() (models.ConversionResponse, error) {
+	var response models.ConversionResponse
+
+	req, err := http.NewRequest("GET", CONV_RATES, nil)
+	if err != nil {
+		return response, err
+	}
+
+	resp, err := t.Client.Do(req)
+	if err != nil {
+		return response, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return response, fmt.Errorf("failed to fetch conversion rates: %s", resp.Status)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
+
+func (t *Tools) saveRates(rates models.ConversionResponse) {
+	stored := models.ConversionStorage{
+		Set:     time.Now(),
+		Expires: time.Now().Add(5 * time.Minute),
+		Rates:   rates.Rates,
+	}
+
+	data, err := json.Marshal(stored)
+	if err != nil {
+		log.Printf("Error marshaling conversion rates: %v\n", err)
+
+		if err := t.Redis.Set(context.Background(), "CNV::GETTING", "0", 0).Err(); err != nil {
+			log.Printf("Error saving conversion rate retrieval in process as false: %v\n", err)
+		}
+	}
+
+	if err := t.Redis.MSet(context.Background(), "CNV::RATES", data, "CNV::GETTING", "0").Err(); err != nil {
+		log.Printf("Error saving conversion rates and retrieval in process as false: %v\n", err)
+	}
+}
+
+func (t *Tools) getRates() (map[string]float64, error) {
+	results, err := t.Redis.MGet(context.Background(), "CNV::RATES", "CNV::GETTING").Result()
+	if err == nil {
+		var stored models.ConversionStorage
+		var getting string
+
+		if results[0] != nil {
+			if err := json.Unmarshal([]byte(results[0].(string)), &stored); err == nil {
+				if stored.Expires.After(time.Now()) {
+					return stored.Rates, nil
+				}
+				if results[1] != nil {
+					getting = results[1].(string)
+					if getting == "1" {
+						return stored.Rates, nil
+					}
+				}
+			}
+		}
+	} else {
+		log.Printf("Error retrieving conversion rates: %v\n", err)
+	}
+
+	if err := t.Redis.Set(context.Background(), "CNV::GETTING", "1", 0).Err(); err != nil {
+		log.Printf("Error setting conversion rate retrieval in process as true: %v\n", err)
+	}
+
+	rates, err := t.getConversionRates()
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		t.saveRates(rates)
+	}()
+
+	return rates.Rates, nil
 }
